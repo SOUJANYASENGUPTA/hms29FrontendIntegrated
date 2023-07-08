349c0bf3c1069cd93e101d7b02bb9aeea4f4da76
SOUJANYASENGUPTA <96814592+SOUJANYASENGUPTA@users.noreply.github.com>
2023-07-08 15:37:31 +0530

    Update Pharmacy.jsx

--- src/pages/pharmacy/Pharmacy.jsx
+++ src/pages/pharmacy/Pharmacy.jsx
@@ -134,7 +134,7 @@ const Pharmacy = () => {
                 <div className='dashbord-header-container'>
                     {!edit && !add && <button className='dashbord-header-btn' onClick={handleNewRecord}>New pharmacy</button>}
                     {(edit || add) && <button className='dashbord-header-btn' onClick={handleBack}>Back to pharmacy</button>}
-                    <div className='dashbord-header-right'>
+                    {/*<div className='dashbord-header-right'>
                         <img
                             src={NotificationIcon}
                             alt='notificationIcon'
@@ -146,7 +146,7 @@ const Pharmacy = () => {
                         <img
                             className='dashbord-header-avatar'
                             src='https://reqres.in/img/faces/9-image.jpg' alt="dashImage" />
-                    </div>
+                    </div>*/}
                 </div>
                 {!edit && !add && <div className='dashboard-content-container'>
                     <div className='dashboard-content-header'>
